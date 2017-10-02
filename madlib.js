ff44379d0d09aa54862ead55ca25e4ee34d6498b
BChrabascz17 <32140897+BChrabascz17@users.noreply.github.com>
2017-10-02 11:18:58 -0400

    Not showing label
    The part of speech part does not work. shows 0,2 instead of part of speech.

--- madlib.js
+++ madlib.js
@@ -1,18 +1,38 @@
+//initialize variables
 var sentence = [];
-var replacement = [];
+var replacements = [];
 var index = 0;
-alert("End with Shift+6(^)");
+//initialize users
 alert("filler go away.");
-//populate(index, sentence);
-alert(populate(index, sentence));
+alert("End with Shift+6(^)");
+//fill sentence
+populate(index, sentence,"word");
+index = 0;
+alert(sentence);
+// specify positions
+populate(index, replacements, "position");
+alert(replacements);
+index = 0;
+populate(index,sentence, replacements, "part of speech");
+alert(sentence);
 
-function populate(sindex, sarray) {
+
+function populate(sindex, sarray, label) {
 	do{
-		sarray[sindex]= prompt("Next word:");
+		sarray[sindex]= prompt("Next "+label+":");
 		sindex = sindex + 1;
 	}
 	while (sarray[sindex - 1]!="^");
 	console.log(sarray);
 	sarray.splice(-1,1);
 	return sarray;
+}
+
+
+
+function replace(sindex, sarray, rarray, label) {
+	for (sindex = 0; sindex < rarray.length; word++){
+		sarray[rarray[sindex]] = prompt("Next "+label+":");
+	}
+	return sarray; 	
 }
